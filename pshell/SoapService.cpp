44adc73de38a543a28b3b020d89d3d0322b40a75
piero.contezini <piero.contezini@15faccbe-6038-11de-a866-630dacd2c331>
2010-04-25 01:56:29 +0000

    SOAP support working

--- pshell/SoapService.cpp
+++ pshell/SoapService.cpp
@@ -8,8 +8,22 @@
  */
 
 #include "SoapService.h"
+#include "XML.h"
 
 CLASSPROXYCALLBACK(SoapService,ListOperations);
+CLASSPROXYCALLBACK(SoapService,SoapCallBack);
+
+static void ExceptionCallBack(void *customPtr,DomElement *Data) {
+//        printf("CALLBACK de Exception Recebida tambŽm!!\n");
+        ((SoapService *)customPtr)->receiveData(Data);
+}
+
+static void SoapServiceCallBack(void *customPtr, DomElement *Data) {
+//        printf("CALLBACK RECEBIDA COM SUCESSO!!\n");
+//        printf("%s\n", Data->dump());
+        ((SoapService *)customPtr)->receiveData(Data);
+}
+
 
 Handle<String> SoapService::getClassName() {
 	HandleScope scope;
@@ -18,14 +32,19 @@ Handle<String> SoapService::getClassName() {
 }
 
 void SoapService::registerCallbacks() {
-	registerFunction("listOperations",GETCLASSPROXYCALLBACK(SoapService,ListOperations));	
+	registerFunction("listOperations",GETCLASSPROXYCALLBACK(SoapService,ListOperations));
+	std::vector<std::string> operations = ((BeerSoapLoader *)loader)->listOperations();
+	for(unsigned int i = 0; i < operations.size(); i++) {
+		registerFunction(operations[i],GETCLASSPROXYCALLBACK(SoapService,SoapCallBack));
+	}
 }
 
 SoapService::SoapService(std::string serviceName, SoapLoader *loader) {
+	this->loader = loader;
 	registerCallbacks();
 	setName(serviceName);
 	setURL(((BeerSoapLoader *)loader)->getServiceURL(serviceName));
-	this->loader = loader;
+	registerExceptionCallBack(ExceptionCallBack, this);
 }
 
 Handle<Object> SoapService::ListOperations(const Arguments& args) {
@@ -39,4 +58,49 @@ Handle<Object> SoapService::ListOperations(const Arguments& args) {
 	}
 	
 	return scope.Close(OperationList);
+}
+
+Handle<Value> SoapService::SoapCallBack(const Arguments& args) {
+	HandleScope scope;
+	Handle<Function> functionObject = args.Callee();
+	Local<Object> self = args.Holder();
+
+	Handle<Value> functionName = functionObject->GetName();
+//	printf("funcao: %s\n", *String::Utf8Value(functionName));
+
+	BeerSoapServiceMethod *method = ((BeerSoapLoader *)loader)->getMethod(*String::Utf8Value(functionName), (BeerSoapService *)this);
+
+	method->registerCallBack(SoapServiceCallBack,this);
+
+	method->setMethodResponse(((BeerSoapLoader *)loader)->getMethodResponse(*String::Utf8Value(functionName), (BeerSoapService *)this));
+
+	std::vector<std::string > parameterList = ((BeerSoapLoader *)loader)->describeMethod((*String::Utf8Value(functionName)));
+
+	if ((unsigned)args.Length() != parameterList.size()) {
+		return v8::ThrowException(v8::String::New("Need Parameters"));
+	}
+
+	for(unsigned int i = 0; i < parameterList.size(); i++) {
+		method->pushParameter(parameterList[i],*String::Utf8Value(args[i]));
+	}
+
+	data = NULL; // aqui nao teria de dar um free antes?
+
+	if(method->run()) {
+		if(data) {
+			XML *newXML = new XML();
+			std::string xmlData = data->dump();
+
+			if(!((DomTree *)newXML)->load((char *)xmlData.c_str(), xmlData.size())) {
+				return v8::ThrowException(v8::String::New("Error Parsing XML"));
+			}
+			return(scope.Close(newXML->registerObject()));
+		}
+	}
+
+	return v8::ThrowException(String::New("Cannot call SOAP method"));
+}
+
+void SoapService::receiveData(DomElement *data) {
+	this->data = data;
 }
